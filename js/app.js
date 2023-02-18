cc522f1705f68ad5515dffae3f1494d5dde5dd19
FKDCQL <121210385+FKDCQL@users.noreply.github.com>
2023-02-18 19:19:09 +0800

    dw

--- js/app.js
+++ js/app.js
@@ -1,6 +1,28 @@
+
 (function (window) {
 	'use strict';
 
-	// Your starting point. Enjoy the ride!
+
+let vm=new Vue({
+el:"#cql",
+data:{
+    txt:"hello msg",
+	inputtxt:""
+},
+methods:{
+	cun(){
+		alert(vm.inputtxt)
+		$.ajax({
+			type: "get",
+			url: "http://localhost:9000/get?name="+vm.inputtxt,
+			success: function(msg){
+			  console.log(msg)
+			}
+		 });
+		 alert("已经储存");
+	}
+}
+})
+
 
 })(window);
